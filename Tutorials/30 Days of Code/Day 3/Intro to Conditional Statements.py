f1819c488a0520e7dae10f70dbb9ebc74a47b34b
JasonJafariHome <jason.jafari.m@gmail.com>
2020-04-13 00:51:30 -0400

    v

---
+++ Tutorials/30 Days of Code/Day 3/Intro to Conditional Statements.py
@@ -0,0 +1,14 @@
+N = int(input())
+ 
+ff = "Weird"
+if N%2== 1:
+    ff = "Weird"
+elif N>=2 and N <5:
+    ff = "Not Weird"
+elif N>=6 and N < 20:
+    ff = "Weird"
+else :
+    ff = "Not Weird"
+print(ff)
+
+
